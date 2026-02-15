9a51976a4ff2a37c4dfc840ccfc15dd5f053c888
Vivian Dai <z5591919@ad.unsw.edu.au>
2026-02-15 22:01:15 +1100

    implement tasks 1-3

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,24 +43,105 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	parsedRecipeName = ""
+	shouldCapitalise = True
+	for char in recipeName:
+		if char == "-" or char == "_" or char.isspace():
+			parsedRecipeName += " "
+			shouldCapitalise = True
+		elif char.isalpha():
+			if shouldCapitalise:
+				parsedRecipeName += char.upper()
+				shouldCapitalise = False
+			else:
+				parsedRecipeName += char.lower()
+
+	if len(parsedRecipeName) <= 0:
+		return None
+	
+	return re.sub(r'\s+', ' ', parsedRecipeName).strip()
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	entry = request.get_json()
+
+	entry_type = entry.get("type")
+	name = entry.get("name")
+
+	if entry_type not in ("recipe", "ingredient"):
+		return '', 400
+	
+	if entry_type == "ingredient" and ("cookTime" not in entry or entry.get("cookTime") < 0):
+		return '', 400
+	
+	if name in cookbook:
+		return '', 400
+	
+	if entry_type == "recipe":
+		if "requiredItems" not in entry:
+			return '', 400
+
+		names = set()
+		for item in entry["requiredItems"]:
+			if item.get("name") in names:
+				return '', 400
+			names.add(item.get("name"))
+
+	cookbook[name] = entry
+	return jsonify({}), 200
 
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	recipeName = request.args.get("name")
+	entry = cookbook.get(recipeName)
+
+	if entry is None or entry["type"] != "recipe":
+		return '', 400
+	
+	ingredientTotals = {}
+	ingredients = []
+	cookTime = 0
+	try:
+		for item in entry["requiredItems"]:
+			buildRecursive(item, ingredientTotals)
+	except ValueError:
+		return '', 400
+	
+	for name, quantity in ingredientTotals.items():
+		ingredient = cookbook.get(name)
+		if ingredient is None:
+			return '', 400
+
+		cookTime += quantity * ingredient["cookTime"]
+		ingredients.append({"name": name, "quantity": quantity})
+	
+	result = {
+		"name": recipeName,
+		"cookTime": cookTime,
+		"ingredients": ingredients
+	}
+
+	return jsonify(result), 200
+
+def buildRecursive(item, ingredientTotals, multiplier=1):
+	name = item["name"]
+	entry = cookbook.get(name)
+	if entry is None:
+		raise ValueError("Missing item")
+	
+	quantity = item["quantity"] * multiplier
+	
+	if entry["type"] == "ingredient":
+		ingredientTotals[name] = ingredientTotals.get(name, 0) + quantity
+	else:
+		for child in entry["requiredItems"]:
+			buildRecursive(child, ingredientTotals, quantity)
 
 
 # =============================================================================
